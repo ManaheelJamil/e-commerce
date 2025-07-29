3b677c7ec52a4cd4182c4ceea326dfa92474ad1d
manaheeljamil <manaheeljamil20@gmail.com>
2025-07-29 11:50:36 +0500

    e-commerce

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+function dashboard(){
+    return(
+        <>
+        <h1>WELCOME TO ADMIN PANNEL</h1>
+        </>
+    )
+}
+export default dashboard
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,68 @@
+'use client'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+export default function Login() {
+    const router = useRouter()
+  const [form, setForm] = useState({ email: '', password: '' })
+
+  const handleChange = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    const res = await fetch('http://localhost:5000/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(form),
+    })
+
+    const data = await res.json()
+
+    if (res.ok) {
+      localStorage.setItem('token', data.token)
+      alert(`Welcome, ${data.user.name}`)
+     router.push('/dashboard')
+    } else {
+      alert(data.message)
+    }
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-6 rounded-lg shadow-md w-full max-w-md space-y-4"
+      >
+        <h2 className="text-2xl font-semibold text-center text-gray-800">Login</h2>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+          required
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required
+          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <div className='flex justify-center'>
+          <h1><Link href="/signup" className='text-sm text-center text-blue-900 underline'>Signup</Link></h1>
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition-colors"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  )
+}
